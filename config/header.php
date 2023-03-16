5bb036bdcdc9f267c2f3c1b87b3e981bb102aa25
basqueoro <basqueroto.felipe125@gmail.com>
2023-03-16 08:20:38 -0300

    criação dos arquivos php

---
+++ config/header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="first">
+    <figure><img src="../img/senai.png" alt="Logo senai Livre"></figure>
+        <div id="divBusca">
+            <input type="text" id="txtBusca" placeholder="Buscar..."/>
+        </div>
+        <nav>
+            <a href="">vender</a>
+            <a href="">contato</a>
+            <a href="">entrar</a>
+            <a href="">criar uma conta</a>
+        </nav>
+        
+    </div>
+    <div class="second">
+        
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include("config/cabecalho.php.php")
+?>
+
+
+
+<?php
+    include("/config/rodape.php")
+?>
